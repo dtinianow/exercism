0fc88b5eaa17169c4f5ff15d99c7514b1ceb33d8
David Tinianow <david.tinianow@gmail.com>
2016-11-21 12:12:34 -0500

    Fix syntax of ruby sieve exercism

--- ruby/sieve/sieve.rb
+++ ruby/sieve/sieve.rb
@@ -10,29 +10,30 @@ class Sieve
     return [] if limit < 2
     find_prime_numbers
   end
-end
+  
+  def find_prime_numbers
+    [*2..limit].each { |num| determine_non_primes(num, num_tracker) }
+    retrieve_primes(num_tracker)
+  end
 
-def find_prime_numbers
-  [*2..limit].each { |num| determine_non_primes(num, num_tracker) }
-  retrieve_primes(num_tracker)
-end
+  def determine_non_primes(num, num_tracker)
+    increment = num
+    while(num <= limit)
+      num += increment
+      num_tracker[num] = 'Not prime' if num <= limit
+    end
+  end
 
-def determine_non_primes(num, num_tracker)
-  increment = num
-  while(num <= limit)
-    num += increment
-    num_tracker[num] = 'Not prime' if num <= limit
+  def retrieve_primes(primes_identifier)
+    primes = primes_identifier.find_all { |key, value| value == 'Prime' }
+    primes.map { |prime| prime.first }
+  end
+
+  def create_num_tracker
+    (2..limit).reduce({}) { |hash, num| hash[num] = 'Prime'; hash }
   end
 end
 
-def retrieve_primes(primes_identifier)
-  primes = primes_identifier.find_all { |key, value| value == 'Prime' }
-  primes.map { |prime| prime.first }
-end
-
-def create_num_tracker
-  (2..limit).reduce({}) { |hash, num| hash[num] = 'Prime'; hash }
-end
 
 module BookKeeping
   VERSION = 1
