03bd053f3f7bf5f3d052d84cb7eb8413d9097c5c
David Tinianow <david.tinianow@gmail.com>
2016-11-29 17:46:10 -0500

    Refactor ruby grains exercism

--- ruby/grains/grains.rb
+++ ruby/grains/grains.rb
@@ -2,10 +2,11 @@ class Grains
   TOTAL_SQUARES = 64
 
   def self.square(n)
+    raise ArgumentError, "Must be between 1 and #{TOTAL_SQUARES}" if n < 1 || n > 64
     2 ** (n - 1)
   end
 
   def self.total
-    (2 ** TOTAL_SQUARES) - 1
+    (1..TOTAL_SQUARES).reduce(0) { |total, n| total += self.square(n) }
   end
 end
