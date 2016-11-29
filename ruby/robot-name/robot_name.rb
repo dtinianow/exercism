08eb809595da4183583a47c416f98959aa98abf0
David Tinianow <david.tinianow@gmail.com>
2016-11-29 17:17:18 -0500

    Refactor robot name ruby based on iteration 1 suggestions

--- ruby/robot-name/robot_name.rb
+++ ruby/robot-name/robot_name.rb
@@ -1,26 +1,32 @@
+RubyVM::InstructionSequence.compile_option = {
+  tailcall_optimization: true,
+  trace_instruction: false
+}
+
 class Robot
   attr_reader :name
   @@used_names = []
 
   def initialize
-    @name = get_name
+    @name = generate_name
   end
 
   def reset
-    initialize
+    @@used_names.delete(name)
+    @name = generate_name
   end
 
-  def get_name
+  private
+
+  def generate_name
     random_name = random_letters + random_numbers
     if @@used_names.include?(random_name)
-      get_name
+      generate_name
     else
       @@used_names << random_name
       random_name
     end
   end
-
-private
 
   def random_letters
     (0...2).map { (65 + rand(26)).chr }.join
@@ -30,6 +36,7 @@ private
     rand(1000).to_s
   end
 end
+
 
 module BookKeeping
   VERSION = 2
