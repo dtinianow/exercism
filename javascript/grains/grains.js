8cb0fa5cf947c31bf64a1f515c4f6dd6ece66aee
David Tinianow <david.tinianow@gmail.com>
2016-12-01 22:35:39 -0700

    Complete javascript grains exercism

---
+++ javascript/grains/grains.js
@@ -0,0 +1,19 @@
+var bigInt = require('./big-integer');
+
+var Grains = function() {
+  this.squares = 64;
+};
+
+Grains.prototype.square = function(num) {
+  return bigInt(2).pow(num - 1).toString();
+};
+
+Grains.prototype.total = function() {
+  var total = bigInt(0);
+  for (var i = 1; i <= this.squares; i++) {
+    total = total.add(this.square(i));
+  }
+  return total.toString();
+}
+
+module.exports = Grains;
