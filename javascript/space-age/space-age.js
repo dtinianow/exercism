bfb385d0f552b61c36021b48a16e9af64aa17361
David Tinianow <david.tinianow@gmail.com>
2016-12-03 13:40:03 -0700

    Refactor javascript space age exercism

--- javascript/space-age/space-age.js
+++ javascript/space-age/space-age.js
@@ -1,7 +1,8 @@
-var SpaceAge = function(seconds) {
+var SpaceAge = function(seconds){
   this.seconds = seconds;
-  this.earthYearInDays = 365.25;
-  this.yearLengthRelativeToEarth = {
+  this.secondsPerEarthYear = 31557600;
+  this.ageOnEarth = seconds / this.secondsPerEarthYear;
+  this.yearLengthRatio = {
     'earth': 1.00,
     'mercury': 0.2408467,
     'venus': 0.61519726,
@@ -13,51 +14,17 @@ var SpaceAge = function(seconds) {
   };
 };
 
-SpaceAge.prototype.onEarth = function() {
-  return this.calculateAge('earth');
-};
+SpaceAge.prototype.onEarth = function(){ return this.calculateAge('earth'); };
+SpaceAge.prototype.onMercury = function(){ return this.calculateAge('mercury'); };
+SpaceAge.prototype.onVenus = function(){ return this.calculateAge('venus'); };
+SpaceAge.prototype.onMars = function(){ return this.calculateAge('mars'); };
+SpaceAge.prototype.onJupiter = function(){ return this.calculateAge('jupiter'); };
+SpaceAge.prototype.onSaturn = function(){ return this.calculateAge('saturn'); };
+SpaceAge.prototype.onUranus = function(){ return this.calculateAge('uranus'); };
+SpaceAge.prototype.onNeptune = function(){ return this.calculateAge('neptune'); };
 
-SpaceAge.prototype.onMercury = function() {
-  return this.calculateAge('mercury');
-};
-
-SpaceAge.prototype.onVenus = function() {
-  return this.calculateAge('venus');
-};
-
-SpaceAge.prototype.onMars = function() {
-  return this.calculateAge('mars');
-};
-
-SpaceAge.prototype.onJupiter = function() {
-  return this.calculateAge('jupiter');
-};
-
-SpaceAge.prototype.onSaturn = function() {
-  return this.calculateAge('saturn');
-};
-
-SpaceAge.prototype.onUranus = function() {
-  return this.calculateAge('uranus');
-};
-
-SpaceAge.prototype.onNeptune = function() {
-  return this.calculateAge('neptune');
-};
-
-SpaceAge.prototype.calculateAge = function(planet) {
-  var planetYearInDays = this.earthYearInDays * this.yearLengthRelativeToEarth[planet];
-  var planetYearInSeconds = this.convertDaysToSeconds(planetYearInDays);
-  return this.ageInSeconds(planetYearInSeconds);
-};
-
-SpaceAge.prototype.convertDaysToSeconds = function(days) {
-  var earthDayInSeconds = 86400;
-  return days * earthDayInSeconds;
-};
-
-SpaceAge.prototype.ageInSeconds = function(planetYearInSeconds) {
-  return +(this.seconds / planetYearInSeconds).toFixed(2);
+SpaceAge.prototype.calculateAge = function(planet){
+  return +((this.ageOnEarth) / this.yearLengthRatio[planet]).toFixed(2);
 };
 
 module.exports = SpaceAge;
