b4a09d23ca6b33b2bd184b41f3400be757feed99
David Tinianow <david.tinianow@gmail.com>
2016-12-06 14:43:27 -0700

    Refactor ruby simple linked list based on suggestions

--- ruby/simple-linked-list/simple_linked_list.rb
+++ ruby/simple-linked-list/simple_linked_list.rb
@@ -1,4 +1,59 @@
-class Element
+class SimpleLinkedList
+  attr_reader :size, :head
+
+  def self.from_a(values)
+    linked_list = new
+    values.reverse_each { |datum| linked_list.push(datum) } if values
+    linked_list
+  end
+
+  def initialize
+    @size = 0
+    @head = nil
+  end
+
+  def empty?
+    size.zero?
+  end
+
+  def push(value)
+    @head = Element.new(value, head)
+    @size += 1
+    self
+  end
+
+  def peek
+    head.datum if head
+  end
+
+  def pop
+    popped_head = head
+    @head = head.next
+    @size -= 1
+    popped_head.datum
+  end
+
+  def each
+    return enum_for(:each) unless block_given?
+    current_node = head
+    until current_node.nil?
+      yield current_node.datum
+      current_node = current_node.next
+    end
+  end
+
+  def to_a
+    each.to_a
+  end
+
+  def reverse
+    each.with_object(SimpleLinkedList.new) { |datum, list| list.push(datum) }
+  end
+
+  alias << push
+end
+
+class SimpleLinkedList::Node
   attr_reader :datum, :next
 
   def initialize(value, next_element = nil)
@@ -11,50 +66,4 @@ class Element
   end
 end
 
-class SimpleLinkedList
-  attr_reader :size, :head
-
-  def initialize
-    @size = 0
-    @head = nil
-  end
-
-  def empty?
-    size.zero?
-  end
-
-  def push(value)
-    @size += 1
-    @head = Element.new(value, head)
-  end
-
-  def peek
-    head.datum if head
-  end
-
-  def pop
-    @size -= 1
-    @head = head.next
-    size + 1
-  end
-
-  def self.from_a(values)
-    linked_list = new
-    values.reverse.each { |datum| linked_list.push(datum) } unless values.nil?
-    linked_list
-  end
-
-  def to_a
-    values = []
-    current_node = head
-    until current_node.nil?
-      values << current_node.datum
-      current_node = current_node.next
-    end
-    values
-  end
-
-  def reverse
-    SimpleLinkedList.from_a(to_a.reverse)
-  end
-end
+Element = SimpleLinkedList::Node
