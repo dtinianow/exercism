8a45cee7c4192a379de32a4c0a7c3faac07f0fd5
David Tinianow <david.tinianow@gmail.com>
2016-12-13 17:43:40 -0700

    Complete ruby sum of multiples

---
+++ ruby/sum-of-multiples/sum_of_multiples.rb
@@ -0,0 +1,20 @@
+class SumOfMultiples
+  def initialize(*multiples)
+    @multiples = multiples
+  end
+
+  def to(limit)
+    (2...limit).reduce(0) do |sum, num|
+      sum += num if multiples.any? { |multiple| is_multiple?(num, multiple) }
+      sum
+    end
+  end
+
+  private
+
+  attr_reader :multiples
+
+  def is_multiple?(num, multiple)
+    (num % multiple).zero?
+  end
+end
