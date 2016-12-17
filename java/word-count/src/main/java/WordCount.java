5c2ebf77231aa406712bc9ce15af6fa089aeacf7
David Tinianow <david.tinianow@gmail.com>
2016-12-17 13:46:54 -0700

    Refactor java word count

--- java/word-count/src/main/java/WordCount.java
+++ java/word-count/src/main/java/WordCount.java
@@ -4,18 +4,12 @@ import java.util.List;
 
 public class WordCount {
 
-    private final Map<String, Integer> wordCount = new HashMap();
-
     public Map<String, Integer> phrase(String phrase) {
+        Map<String, Integer> wordCount = new HashMap();
         String[] words = phrase.replaceAll("[^a-zA-Z0-9 ]", "").split("\\s+");
         for (String word : words) {
             String formattedWord = word.toLowerCase();
-            Integer count = wordCount.get(formattedWord);
-            if (count == null) {
-                wordCount.put(formattedWord, 1);
-            } else {
-                wordCount.put(formattedWord, count + 1);
-            }
+            wordCount.put(formattedWord, wordCount.getOrDefault(formattedWord, 0) + 1);
         }
         return wordCount;
     }
