4fc47e3577beb74de44f02dc0607179f6df93520
David Tinianow <david.tinianow@gmail.com>
2017-01-16 20:27:33 -0700

    Refactor javascript raindrops

--- javascript/raindrops/raindrops.js
+++ javascript/raindrops/raindrops.js
@@ -2,13 +2,12 @@ var Raindrops = function() {};
 
 Raindrops.prototype.convert = function(input) {
   var output = '';
-  
+
   if (input % 3 === 0) output += 'Pling';
   if (input % 5 === 0) output += 'Plang';
   if (input % 7 === 0) output += 'Plong';
-  if (output === '') output += input;
 
-  return output;
+  return output || input.toString();
 };
 
 module.exports = Raindrops;
