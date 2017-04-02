3345b5992becb5e1da3d35f8c9c0d44793b68fab
David Tinianow <david.tinianow@gmail.com>
2017-04-01 18:54:06 -0600

    Refactor javascript accumulate

--- javascript/accumulate/accumulate.js
+++ javascript/accumulate/accumulate.js
@@ -1,8 +1,7 @@
-function accumulate(items, func) {
-  const acc = [];
-  for (let i = 0; i < items.length; i++)
-    acc.push(func(items[i]));
-  return acc;
+function accumulate(items, callback) {
+  const results = [];
+  items.forEach(item => results.push(callback(item)));
+  return results;
 }
 
 module.exports = accumulate;
