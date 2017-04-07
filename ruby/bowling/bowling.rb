59224ad92dcdf0634301362b17c4f3c8db0a7fa5
David Tinianow <david.tinianow@gmail.com>
2017-04-06 20:21:32 -0600

    Begin building ruby bowling

---
+++ ruby/bowling/bowling.rb
@@ -0,0 +1,72 @@
+class Game
+  attr_reader :frames
+
+  def initialize
+    @frames = []
+    start_new_frame
+  end
+
+  def roll(pins)
+    raise BowlingError if pins < 0
+    raise BowlingError if pins > 10
+    raise BowlingError if !frames.last.strike? && frames.last.rolls.reduce(0, :+) > 10
+
+    if frames.length > 1 && frames[-2].strike? && frames[-2].bonus < 20
+      frames[-2].add_bonus(pins)
+    end
+    if (frames.last.spare? || frames.last.strike?) && frames.length != 10
+      frames.last.add_bonus(pins)
+      start_new_frame
+    elsif frames.last.open?
+      start_new_frame
+    end
+    frames.last.add_roll(pins)
+  end
+
+  def score
+    raise BowlingError if frames.length != 10
+    frames.reduce(0) { |sum, frame| sum + frame.points }
+  end
+
+  private
+
+  def start_new_frame
+    @frames << Frame.new
+  end
+end
+
+class Frame
+  attr_reader :rolls, :bonus
+
+  def initialize
+    @rolls = []
+    @bonus = 0
+  end
+
+  def points
+    rolls.reduce(0, :+) + bonus
+  end
+
+  def add_roll(pins)
+    @rolls << pins
+  end
+
+  def add_bonus(pins)
+    @bonus += pins
+  end
+
+  def strike?
+    rolls.length > 0 && rolls.length < 3 && rolls.first == 10
+  end
+
+  def spare?
+    rolls.length > 1 && rolls.length < 3 && rolls.first + rolls.last == 10
+  end
+
+  def open?
+    rolls.length == 2 && rolls.first + rolls.last < 10
+  end
+end
+
+class BowlingError < StandardError
+end
