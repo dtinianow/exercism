8a8a0155dd287bc27381184362f64a53d8d8591d
David Tinianow <david.tinianow@gmail.com>
2017-04-09 15:59:04 -0600

    Refactor ruby bowling

--- ruby/bowling/bowling.rb
+++ ruby/bowling/bowling.rb
@@ -7,30 +7,15 @@ class Game
   end
 
   def roll(pins)
-    raise BowlingError if pins < 0 || pins > 10
-    raise BowlingError if !frames.last.strike? && frames.length != 10 && frames.last.rolls.reduce(0, :+) > 10
-    if frames.length == 10 && frames.last.strike? && frames.last.rolls.length == 2 && frames.last.rolls[1] < 10
-      raise BowlingError if pins >= 10
-      raise BowlingError if frames.last.rolls[1] < 10 && frames.last.rolls[1] + pins > 10
-    end
-
-    if frames.length > 1 && frames[-2].strike? && frames[-2].bonus < 20
-      frames[-2].add_bonus(pins)
-    end
-    if (frames.last.spare? || frames.last.strike?) && frames.length != 10
-      frames.last.add_bonus(pins)
-      start_new_frame
-    elsif frames.last.open?
-      start_new_frame
-    end
+    validate_roll(pins)
+    add_bonus_for_previous_frame_strike(pins)
+    add_current_frame_bonus_or_start_new_frame(pins)
     frames.last.add_roll(pins)
   end
 
   def score
-    if frames.length == 10 && (frames.last.strike? || frames.last.spare?)
-      raise BowlingError if frames.last.rolls.length < 3
-    end
-    raise BowlingError if frames.length != 10
+    validate_all_frames_completed
+    validate_final_frame_bonus_rolls_completed
     frames.reduce(0) { |sum, frame| sum + frame.points }
   end
 
@@ -38,6 +23,52 @@ class Game
 
   def start_new_frame
     @frames << Frame.new
+  end
+
+  def validate_roll(pins)
+    validate_pins_knocked_down(pins)
+    validate_strike_score(pins)
+    validate_final_frame_bonus_pins(pins)
+  end
+
+  def validate_pins_knocked_down(pins)
+    raise BowlingError if pins < 0 || pins > 10
+  end
+
+  def validate_strike_score(pins)
+    raise BowlingError if !frames.last.strike? && frames.length != 10 && frames.last.rolls.reduce(0, :+) > 10
+  end
+
+  def validate_final_frame_bonus_pins(pins)
+    if frames.length == 10 && frames.last.strike? && frames.last.rolls.length == 2 && frames.last.rolls[1] < 10
+      raise BowlingError if pins >= 10
+      raise BowlingError if frames.last.rolls[1] < 10 && frames.last.rolls[1] + pins > 10
+    end
+  end
+
+  def validate_final_frame_bonus_rolls_completed
+    if frames.length == 10 && (frames.last.strike? || frames.last.spare?)
+      raise BowlingError if frames.last.rolls.length < 3
+    end
+  end
+
+  def validate_all_frames_completed
+    raise BowlingError if frames.length != 10
+  end
+
+  def add_bonus_for_previous_frame_strike(pins)
+    if frames.length > 1 && frames[-2].strike? && frames[-2].bonus < 20
+      frames[-2].add_bonus(pins)
+    end
+  end
+
+  def add_current_frame_bonus_or_start_new_frame(pins)
+    if (frames.last.spare? || frames.last.strike?) && frames.length != 10
+      frames.last.add_bonus(pins)
+      start_new_frame
+    elsif frames.last.open?
+      start_new_frame
+    end
   end
 end
 
