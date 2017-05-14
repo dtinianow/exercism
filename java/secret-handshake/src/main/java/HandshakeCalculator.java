9ad54f4b4603da4bcb488686a96149312f725479
David Tinianow <david.tinianow@gmail.com>
2017-05-13 22:47:10 -0400

    Complete java secret handshake

---
+++ java/secret-handshake/src/main/java/HandshakeCalculator.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+final class HandshakeCalculator {
+
+    private static final Map<Integer, Signal> BINARY_TO_ACTION = new LinkedHashMap<>();
+    static {
+        BINARY_TO_ACTION.put(1, Signal.WINK);
+        BINARY_TO_ACTION.put(2, Signal.DOUBLE_BLINK);
+        BINARY_TO_ACTION.put(4, Signal.CLOSE_YOUR_EYES);
+        BINARY_TO_ACTION.put(8, Signal.JUMP);
+    }
+
+    public List<Signal> calculateHandshake(int decimal) {
+        List<Signal> handshake = new ArrayList<>();
+
+        for (Map.Entry<Integer, Signal> entry: BINARY_TO_ACTION.entrySet()) {
+            if ((decimal & entry.getKey()) != 0) {
+                handshake.add(entry.getValue());
+            }
+        }
+
+        if ((decimal & 16) != 0) {
+            Collections.reverse(handshake);
+        }
+
+        return handshake;
+    }
+}
